00a964d1d13bb0af20de0f20a21e1cba0c7b6bc9
cherry1511 <30823868+cherry1511@users.noreply.github.com>
2017-08-15 16:17:46 +0700

    Add Espresso Class and Housblend class

---
+++ src/HousBlend.java
@@ -0,0 +1,15 @@
+
+public class HousBlend extends Beverage {
+
+	public HousBlend() {
+		description = "House Blend Coffee";
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return 0.89;
+	}
+
+}
